7f10e9f6293277563b5c6242319d614c5e72705b
Alex <1571478933@qq.com>
2026-03-20 18:06:36 +0800

    用户信息修改

---
+++ component/component_common/src/main/java/com/wkq/common/CommonRoutes.kt
@@ -0,0 +1,12 @@
+package com.wkq.common
+
+import com.wkq.core.router.Route
+import com.wkq.core.router.RouteEntry
+
+
+object CommonRoutes {
+
+    val routes = listOf<RouteEntry.ActivityEntry>(
+
+    )
+}
---
+++ component/component_common/src/main/java/com/wkq/common/xx.java
@@ -0,0 +1,11 @@
+package com.wkq.common;
+
+/**
+ *
+ * @ Author: wkq
+ * @ Time: 2026/3/20 16:52
+ * @ Desc:
+ */
+public class xx
+{
+}
